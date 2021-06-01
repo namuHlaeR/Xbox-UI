3c874b6aa3d4335598aeeaeefb894d59e6b45543
namuHlaeR <77580956+namuHlaeR@users.noreply.github.com>
2021-06-01 11:56:52 +0200

    Deatail addition - home screen

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello')
+// DOM component selectors
+const box0 = document.querySelector('.box0');
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
+const box5 = document.querySelector('.box5');
+const box6 = document.querySelector('.box6');
+const box7 = document.querySelector('.box7');
+const box8 = document.querySelector('.box8');
+const box9 = document.querySelector('.box9');
+const box10 = document.querySelector('.box10');
+const box11 = document.querySelector('.box11');
+
+box0.classList.add('hidden');
+// Event listeners
+
+
+
+// Functions
